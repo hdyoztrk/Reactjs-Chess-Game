16164f58b938c411015bbf593477d99d6d6b8be5
hdyoztrk <hediyeoztr@gmail.com>
2023-03-14 23:44:48 +0300

    Starter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+import Board from "./Board";
+import Game from "./Game";
 
 function App() {
+  console.log(Game);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="App">
+        <Board />
+      </div>
+    </DndProvider>
   );
 }
 
